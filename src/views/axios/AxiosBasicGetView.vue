e1520343666f33be560f7658685175635de26fd4
DESKTOP-VQVDMU6\USER <nqwrt@naver.com>
2023-01-31 12:50:59 +0900

    axios

---
+++ src/views/axios/AxiosBasicGetView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-for="post in posts" :key="post.id">
+    <h2>{{ post.id }} {{ post.title }}</h2>
+    <p>{{ post.body }}</p>
+  </div>
+</template>
+
+<script>
+//https://www.koderhq.com/tutorial/vue/http-axios/ 예제 참고
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  mounted() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+      console.log(response)
+      console.log(response.data)
+      this.posts = response.data
+    })
+  }
+}
+</script>
+
+<style></style>
---
+++ src/views/axios/AxiosBasicPostView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  mounted() {
+    axios
+      .post('https://jsonplaceholder.typicode.com/posts', {
+        userId: '1',
+        title: 'Article title',
+        body: 'Article body content'
+      })
+      .then((response) => console.log(response))
+  }
+}
+</script>
